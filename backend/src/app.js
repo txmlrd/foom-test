dbd48fea42dcf027e718b6af4f09c365d9f99703
txmlrd <gungadhisanjaya@gmail.com>
2025-11-29 22:12:51 +0700

    fix: adding prefix url

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,24 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const sequelize = require("./config/database");
+
+const productRoutes = require("./routes/productRoutes");
+const stockRoutes = require("./routes/stockRoutes");
+const purchaseRequestRoutes = require("./routes/purchaseRequestRoutes");
+const webhookRoutes = require("./routes/webhookRoutes");
+const warehouseRoutes = require("./routes/warehouseRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/warehouses", warehouseRoutes);
+app.use("/products", productRoutes);
+app.use("/stocks", stockRoutes);
+app.use("/purchase", purchaseRequestRoutes);
+app.use("/webhook", webhookRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log("Server running on port", process.env.PORT);
