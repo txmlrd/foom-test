1b5334fcb33ee303400dee2f07ee868a04d80b8d
txmlrd <gungadhisanjaya@gmail.com>
2025-11-29 22:12:11 +0700

    feat: base main

---
+++ backend/src/app.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log("Server running on port", process.env.PORT);
+});
